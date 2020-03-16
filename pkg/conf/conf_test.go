109ae9b03a380b291f82f8f30404bd990ef34a08
Roman Grytskiv <roman.grytskiv@gmail.com>
2020-03-16 16:44:26 +0100

    Revert "Fix output buffer flushing. (#22)"
    This reverts commit 91433b70729e05674d5823e2463a35c2f78b15ed.

--- pkg/conf/conf_test.go
+++ pkg/conf/conf_test.go
@@ -19,16 +19,15 @@ func TestConfSimple(t *testing.T) {
 	HostQueueSize = 10
 	WorkerPoolSize = 10
 	TCPOutBufSize = 11
-	TCPOutBufFlushPeriodSec = 3
-	MaxHostReconnectPeriodMs = 777
-	HostReconnectPeriodDeltaMs = 13`
+	TCPOutBufFlushPeriodSec = 3`
 
 	expected := Main{
 		ListeningPort: 2003,
 		TargetPort:    2008,
 
-		MainQueueSize:  100,
-		HostQueueSize:  10,
+		MainQueueSize: 100,
+		HostQueueSize: 10,
+
 		WorkerPoolSize: 10,
 
 		IncomingConnIdleTimeoutSec: 13,
@@ -37,8 +36,6 @@ func TestConfSimple(t *testing.T) {
 		TermTimeoutSec:             11,
 		TCPOutBufSize:              11,
 		TCPOutBufFlushPeriodSec:    3,
-		MaxHostReconnectPeriodMs:   777,
-		HostReconnectPeriodDeltaMs: 13,
 
 		NormalizeRecords:  true,
 		LogSpecialRecords: true,
