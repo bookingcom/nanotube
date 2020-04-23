cb1696890904095e361d4cb6b185f754ded07252
Roman Grytskiv <3339062+grzkv@users.noreply.github.com>
2020-04-23 14:22:24 +0200

    Apply suggestions from code review
    Co-Authored-By: Andrei Vereha <avereha@users.noreply.github.com>

--- pkg/target/host.go
+++ pkg/target/host.go
@@ -155,8 +155,7 @@ func (h *Host) Flush(d time.Duration) {
 			return
 		case <-t.C:
 			h.CWm.Lock()
-			if h.Conn != nil && h.W != nil {
-				if h.W.Buffered() != 0 {
+			if h.Conn != nil && h.W != nil && h.W.Buffered() != 0 {
 					err := h.W.Flush()
 					if err != nil {
 						h.Lg.Error("error while flushing the host buffer", zap.Error(err), zap.String("host name", h.Name), zap.Uint16("host port", h.Port))
