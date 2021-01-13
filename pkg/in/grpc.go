dccbef0e508529354b7a9b2af0834b29ad5ae824
Roman Grytskiv <roman.grytskiv@gmail.com>
2021-01-13 17:35:19 +0100

    Added correct handling of stream closure on the server.

--- pkg/in/grpc.go
+++ pkg/in/grpc.go
@@ -42,7 +42,7 @@ type streamerServer struct {
 }
 
 // This function can be running in multiple goroutines simultaneously. Each new incoming
-// transmission will make a new instance.
+// transmission will call a new instance.
 func (server *streamerServer) Stream(s grpcstreamer.Streamer_StreamServer) error {
 	res := grpcstreamer.Result{}
 
@@ -60,17 +60,22 @@ loop:
 		rec, err := s.Recv()
 
 		if err == io.EOF {
+			server.lg.Info("got EOF")
 			// TODO: Do we have to do something special to re-start listening?
 			err := s.SendAndClose(&res)
 			if err != nil {
 				server.lg.Error("error when sending a response to stream", zap.Error(err))
 			}
-		}
-		if err != nil {
+			return nil
+		} else if err != nil {
 			res.ErrorCount++
+			server.lg.Error("error receiving record", zap.Error(err))
 			// TODO: Is it ok to continue after an error?
 			continue
 		}
+		// TODO: Cleanup
+		server.lg.Info("got record", zap.ByteString("rec", rec.Rec))
+
 		res.ReceivedCount++
 		server.queue <- string(rec.Rec)
 	}
