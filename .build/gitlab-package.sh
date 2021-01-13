5a81e9dd9023bd33f2213971f668ceaac9c5a4c6
dams <damien.krotkine@booking.com>
2021-01-13 12:09:23 +0100

    . add a validate stage . use rpmbuild API to build packages . use single docker image for all jobs . build for centos 7 & 8 . build nanotube and subnanotube packages . various fix

--- .build/gitlab-package.sh
+++ .build/gitlab-package.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 if [ -n "${1}" ]
     then CI_PROJECT_NAME=$1
@@ -6,15 +6,15 @@ fi
 
 echo "Submitting packaging job"
 
-JOB_ID="$(curl -X POST -H 'Content-Type: application/json' -H "X-API-KEY: $PACKAGING_API_KEY" -d "{ \"owner\": \"$GITLAB_USER_EMAIL\", \"release_pkg\": 1, \"pkg_scm_url\": \"git@gitlab.booking.com:$CI_PROJECT_PATH.git\", \"pkg_scm_rev\":\"tag::$CI_COMMIT_TAG\", \"spec_scm_dir\": \"packages.git::$CI_PROJECT_NAME\", \"centos\": \"6,7\" }" http://yum-master.prod.booking.com/api/builds/new?src_from_scm=1 | jq .job_id)"
+JOB_ID="$(curl -X POST -H 'Content-Type: application/json' -H "X-API-KEY: $PACKAGING_API_KEY" -d "{ \"owner\": \"$GITLAB_USER_EMAIL\", \"release_pkg\": 1, \"pkg_scm_url\": \"git@gitlab.booking.com:$CI_PROJECT_PATH.git\", \"pkg_scm_rev\":\"tag::$CI_COMMIT_TAG\", \"spec_scm_dir\": \"packages.git::$CI_PROJECT_NAME\", \"centos\": \"7,8\" }" http://yum-master.prod.booking.com/api/builds/new?src_from_scm=1 | tee /dev/fd/2 | jq .job_id)"
 
 [[ -z $JOB_ID ]] && (echo "Problem sending build request!!" && exit 1)
 
 echo "Job submitted with id $JOB_ID"
 
 while true; do
-    for centos_version in 6 7; do
-        if [[ ${done[$centos_version]} -eq 1 ]]; then
+    for centos_version in 7 8; do
+        if [[ ${jobdone[$centos_version]} -eq 1 ]]; then
             continue
         fi
         echo ===centos${centos_version}===
@@ -22,10 +22,10 @@ while true; do
 
         jq ' {id: .id, bpackage_log: .bpackage_log, build_state: .build_state}' <<<$response
 
-        [[ $(jq .build_state <<<$response) =~ done|failed ]] && echo ===centos${centos_version}=== finished && done[$centos_version]=1
+        [[ $(jq .build_state <<<$response) =~ done|failed ]] && echo ===centos${centos_version}=== finished && jobdone[$centos_version]=1
     done
-    [[ ${done[6]} -eq 1 && ${done[7]} -eq 1 ]] && break
-    sleep 30
+    [[ ${jobdone[7]} -eq 1 && ${jobdone[8]} -eq 1 ]] && break
+    sleep 15
 done
 
 response=$(curl -s -X GET -H 'Content-Type: application/json' -H "X-API-KEY: $PACKAGING_API_KEY" http://yum-master.prod.booking.com/api/builds/list/by_key?job_id=$JOB_ID | jq  '.builds[] | {id: .id, bpackage_log: .bpackage_log, build_state: .build_state, centos: .centos, rpms_built: .rpms_built}' | sed -e 's/\/data0/http:\/\/yum-master.prod.booking.com/g')
