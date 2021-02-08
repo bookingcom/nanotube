30792d1ae8c666eecd40d2ee36bcc869740411e1
dams <damien.krotkine@booking.com>
2021-02-08 23:11:38 +0100

    end-to-end testing test2

--- test/run.sh
+++ test/run.sh
@@ -5,27 +5,69 @@
 # Expects compiled sender, receiver, and nanotube in corresponding dirs.
 # set -e
 
-for d in test*/ ; do
-    tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'tmpd') # portable for both Linux and Darwin
-    trap '{ rm -r $tmpdir; }' EXIT
-    ../nanotube -clusters "$d/nanotube/clusters.toml" -config "$d/nanotube/config.toml" -rules "$d/nanotube/rules.toml" &
-    ntPID=$!
+PIDS=''
+trap_pid() {
+    PIDS+="$1 "
+    trap '{ echo "*** force-killing $PIDS ***"; kill -9 $PIDS 2>/dev/null; exit 255; }' SIGINT SIGTERM ERR SIGKILL
+}
 
-    ./receiver/receiver -ports "$(ls -x "$d/out")" -outdir "$tmpdir" &
+for d in test* ; do
+    echo -e "\n*** testing ${d} ***"
+    tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'tmpd') # portable for both Linux and Darwin
+
+    echo -e "\n. tempdir: ${tmpdir}"
+
+    echo -en "\n. starting nanotube..."
+    cd "${d}/nanotube/"
+    ../../../nanotube -config="config.toml" &
+    ntPID=$!
+    trap_pid ${ntPID}
+    echo -e "\r. starting nanotube: pid ${ntPID}"
+    cd ../..
+
+    echo -en "\n. starting receiver..."
+    ./receiver/receiver -ports "$(ls -x "${d}/out")" -outdir "$tmpdir" &
     recPID=$!
-    trap '{ kill $recPID; }' EXIT
+    trap_pid ${recPID}
+    echo -e "\r. starting receiver: pid ${recPID}"
 
     # wait for receiver to start
     sleep 1
-    ./sender/sender -data "$d/in" -host localhost -port 2003
+    if [ -e ${d}/in.bz2 ] && [ ! -f ${d}/in ]; then
+        echo -e "\n. decompressing input"
+        rm -rf ${d}/in
+        bunzip2 ${d}/in.bz2 -c > ${d}/in
+    fi
+
+    if [ -e ${d}/out.tar.bz2 ] && [ ! -d ${d}/out ]; then
+        echo -e "\n. decompressing output"
+        rm -rf ${d}/out
+        tar -C ${d} -jxf ${d}/out.tar.bz2
+    fi
+
+    echo -e "\n. starting sender"
+    ./sender/sender -data "${d}/in" -host localhost -port 2003
 
     # wait for records to propagate through receiver
-    sleep 1
+    sleep 5
+
     kill $ntPID
     wait $ntPID
 
-    if ! diff -qr "${d}out" "$tmpdir"; then
-        exit 1
+    sleep 5
+
+    kill $recPID
+    wait $recPID
+    
+    rm -f ${tmpdir}/in
+
+    echo -e "\n. sorting"
+    for i in $tmpdir/*; do sort -o $i $i; done
+
+    echo -e "\n. comparing"
+    if ! diff -qr "${d}/out" "$tmpdir"; then
+        echo "   -> FAIL: ${tmpdir} and ${d}/out are different"
+    else
+        echo "   -> SUCCESS: ${tmpdir} and ${d}/out are identical"
     fi
 done
-
