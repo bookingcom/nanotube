d802263fa4449bfbdd5f203e0fe349fffbbb884f
grzkv <roman.grytskiv@gmail.com>
2022-04-01 18:05:31 +0200

    Performance benchmarking wrap-up.

--- test/performance/pre-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# pre-setup.sh
-
-# check if hosts are set
-
-# check if hosts are available
--- test/performance/run-sender.sh
+++ test/performance/run-sender.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-
+./sender -data '/etc/in' -host '<nanotube_host>' -port 2003 -rate 1000 -cycle
