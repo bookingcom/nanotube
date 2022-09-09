fb1a270af767303bd0c74c6a9f4a003092319b14
Roman Grytskiv <3339062+grzkv@users.noreply.github.com>
2022-09-09 16:58:57 +0200

    Added check and corresponding test for the consistency of host indices in the jump cluster.

--- pkg/conf/clusters_test.go
+++ pkg/conf/clusters_test.go
@@ -79,3 +79,72 @@ func TestClustersSimple(t *testing.T) {
 		t.Errorf("Expected and factual configs differ:\n%s", diff)
 	}
 }
+
+func TestClustersInvalidIndices(t *testing.T) {
+	clsConfig1 :=
+		`[[cluster]]
+		name = "aaa"
+		type = "jump"
+			[[cluster.hosts]]
+			name = "host1"
+			index = 0
+			port = 123
+			[[cluster.hosts]]
+			name = "host2"
+			index = 1
+			port = 456
+
+		[[cluster]]
+		name = "bbb"
+		type = "jump"
+			[[cluster.hosts]]
+			name = "host11"
+			index = 2
+			port = 234
+			[[cluster.hosts]]
+			name = "host12"
+			index = 1
+			port = 567
+			[[cluster.hosts]]
+			name = "host13"
+			index = 3
+			port = 567`
+
+	clsConfig2 :=
+		`[[cluster]]
+		name = "aaa"
+		type = "jump"
+			[[cluster.hosts]]
+			name = "host1"
+			index = 0
+			port = 123
+			[[cluster.hosts]]
+			name = "host2"
+			index = 1
+			port = 456
+
+		[[cluster]]
+		name = "bbb"
+		type = "jump"
+			[[cluster.hosts]]
+			name = "host11"
+			index = 2
+			port = 234
+			[[cluster.hosts]]
+			name = "host12"
+			index = 1
+			port = 567
+			[[cluster.hosts]]
+			name = "host13"
+			index = 1
+			port = 567`
+
+	_, err := ReadClustersConfig(strings.NewReader(clsConfig1))
+	if err == nil {
+		t.Errorf("reading config succeeded, but failure is expected")
+	}
+	_, err = ReadClustersConfig(strings.NewReader(clsConfig2))
+	if err == nil {
+		t.Errorf("reading config succeeded, but failure is expected")
+	}
+}
